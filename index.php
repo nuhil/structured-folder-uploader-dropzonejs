c32fbee41547adf7b0fa45bf0cc7b5200ec521c4
Nuhil Mehdy <nuhil@nuhil.net>
2014-04-05 17:49:16 +0600

    added all files
    added all files

---
+++ index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<head>
+    <meta charset="UTF-8">
+    <title>HTML5|Folder uploader, keeping recursive directory structure</title>
+    <link href="css/dropzone.css" type="text/css" rel="stylesheet" />
+</head>
+<body>
+    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+    <script>window.jQuery || document.write('<script src="assets/js/vendor/jquery-1.9.1.min.js"><\/script>')</script>
+
+    <script src="js/dropzone.js"></script>
+    
+    <form action="upload.php" class="dropzone" id="uploadFile" name="uploadFile" method="POST">
+        <span id="tmp-path"></span>
+    </form>
+    
+    <script>
+        $(document).ready(function () {
+            Dropzone.autoDiscover = false;
+            
+            Dropzone.options.uploadFile = {
+              init: function() {
+                this.on("success", function(file, responseText) {
+                  file.previewTemplate.appendChild(document.createTextNode(responseText));
+                });
+                
+                this.on("sending", function(file) {
+                    $("#tmp-path").html('<input type="hidden" name="path" value="'+file.fullPath+'" />')
+                });            
+              }
+            }; 
+            
+            var myDropzone = new Dropzone("#uploadFile", { 
+                url: "upload.php"
+            });               
+            
+        });
+    </script>
+
+</body>
+</html>
---
+++ upload.php
@@ -0,0 +1,26 @@
+<?php
+    $ds = DIRECTORY_SEPARATOR;
+    $storeFolder = 'uploads/';
+     
+    if (!empty($_FILES)) {
+         
+        $tempFile = $_FILES['file']['tmp_name'];
+          
+        $targetPath = dirname( __FILE__ ) . $ds . $storeFolder . $ds;
+        
+        $fullPath = $storeFolder.rtrim($_POST['path'], "/.");
+        $folder = substr($fullPath, 0, strrpos($fullPath, "/"));
+        
+        if (!is_dir($folder)) {
+            $old = umask(0);
+            mkdir($folder, 0777, true);
+            umask($old);
+        }    
+        
+        if (move_uploaded_file($tempFile, $fullPath)) {
+            die($fullPath);
+        } else {
+            die('e');
+        }
+    }
+?>
